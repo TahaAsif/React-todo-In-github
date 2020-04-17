651231eb20b086edd7da334080b9ba53764d4c33
TahaAsif <k152855@nu.edu.pk>
2020-04-17 12:19:02 +0500

    Updates

---
+++ precache-manifest.b9756a7be03404bdbfc54eed0fe746c2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60f299d20f76f6f10bfb7d427311bb89",
+    "url": "/React-todo-In-github/index.html"
+  },
+  {
+    "revision": "8e903dd39752b101063d",
+    "url": "/React-todo-In-github/static/css/main.98700b13.chunk.css"
+  },
+  {
+    "revision": "0a18d1502b468d1a4b93",
+    "url": "/React-todo-In-github/static/js/2.88c4bf8b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-todo-In-github/static/js/2.88c4bf8b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e903dd39752b101063d",
+    "url": "/React-todo-In-github/static/js/main.7e09e90e.chunk.js"
+  },
+  {
+    "revision": "d30c243f46e2658d940e",
+    "url": "/React-todo-In-github/static/js/runtime-main.09f67f54.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React-todo-In-github/static/media/logo.5d5d9eef.svg"
+  }
+]);
